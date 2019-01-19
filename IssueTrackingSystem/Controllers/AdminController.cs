88f6c3ac30f8e5a2db5be5d9bfcc0e80883cc0c8
Marcin Kozikowski <marcinkozikowski@wp.pl>
2019-01-19 13:06:59 +0100

    dodany panel administarcyjny

---
+++ IssueTrackingSystem/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueTrackingSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Adres()
+        {
+            return RedirectToAction("Index", "BOK_ADRES");
+        }
+
+        public ActionResult Kontakty()
+        {
+            return RedirectToAction("Index", "BOK_DANE_KONTAKTOWE");
+        }
+
+        public ActionResult Klienci()
+        {
+            return RedirectToAction("Index", "BOK_KLIENT");
+        }
+        public ActionResult Odpowiedz()
+        {
+            return RedirectToAction("Index", "BOK_ODPOWIEDZ");
+        }
+
+        public ActionResult Operator()
+        {
+            return RedirectToAction("Index", "BOK_OPERATOR");
+        }
+        public ActionResult Priorytet()
+        {
+            return RedirectToAction("Index", "BOK_PRIORYTET");
+        }
+
+        public ActionResult Status()
+        {
+            return RedirectToAction("Index", "BOK_STATUS");
+        }
+        public ActionResult Uzytkownik()
+        {
+            return RedirectToAction("Index", "BOK_UZYTKOWNIK");
+        }
+
+        public ActionResult Zalacznik()
+        {
+            return RedirectToAction("Index", "BOK_ZALACZNIK");
+        }
+        public ActionResult Zgloszenie()
+        {
+            return RedirectToAction("Index", "BOK_ZGLOSZENIE");
+        }
+
+    }
+}
